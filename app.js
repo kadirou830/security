ade68e74043907196b5e35feee20c75551655130
hichour abdelkader <kadirou830@gmail.com>
2023-02-26 23:22:16 +0100

    final project

--- app.js
+++ app.js
@@ -8,6 +8,7 @@
    const passport = require('passport');
    const session = require('express-session');
    const GoogleStrategy = require('passport-google-oauth20').Strategy;
+   const FacebookStrategy = require('passport-facebook').Strategy;
    const findOrCreate = require('mongoose-findorcreate');
 
    require('dotenv').config()
@@ -38,7 +39,9 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB",
 );
 
    const userSchema = new mongoose.Schema({
-      googleId: String
+      googleId: String,
+      facebookId: String,
+      secret: String
    })
 
    userSchema.plugin(passportLocalMongoose);
@@ -69,6 +72,18 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB",
     }
   ));
 
+  passport.use(new FacebookStrategy({
+   clientID: process.env.FACEBOOKAPP_ID,
+   clientSecret: process.env.FACEBOOKSECRET_ID,
+   callbackURL: "http://localhost:3000/auth/facebook/secrets",
+ },
+ function(accessToken, refreshToken, profile, cb) {
+   User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+     return cb(err, user);
+   });
+ }
+));
+
    app.get('/', (req, res)=>{
     res.render('home');
    })
@@ -83,6 +98,18 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB",
     res.redirect('/secrets');
   });
 
+
+  app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+  app.get('/auth/facebook/secrets', 
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
+
    app.get('/register', (req, res)=>{
     res.render('register');
    })
@@ -92,12 +119,21 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB",
    })
 
    app.get('/secrets', (req, res)=>{
+         User.find({'secret': {$ne: null}}, (err, foundusers) => {
+            if(err){
+               console.log(err)
+            }else{
+               res.render('secrets', {usersWithSecrets: foundusers});
+            }
+         });   
+   });
+
+   app.get('/submit', (req, res)=>{
       if (req.isAuthenticated()){
-         res.render('secrets');         
+         res.render('submit');         
       }else{
          res.redirect('/login');
       }
-
    })
 
    app.get('/logout', (req, res)=>{
@@ -138,6 +174,20 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB",
             }
          })
    });
+
+   app.post('/submit', (req, res)=>{
+         const mySecret = req.body.secret;
+         User.findById(req.user.id, (err, founduser)=>{
+            if(err){console.log(err)}else{
+               founduser.secret = mySecret;
+               founduser.save((err)=>{
+                  if(err){console.log(err)}else{
+                     res.redirect('/secrets');
+                  }
+               });
+            }
+         });
+   })
 
 
    app.listen(3000, ()=>{
